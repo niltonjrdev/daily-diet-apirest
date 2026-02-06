fe05b6753bf9600c5fdb0bc7be5d5525f3e2cb96
niltonjrdev <dev40.nilton@gmail.com>
2026-02-05 22:31:29 -0300

    test: setup vitest and supertest environment

--- env/index.ts
+++ env/index.ts
@@ -1,5 +1,11 @@
+import dotenv from 'dotenv'
 import { z } from 'zod'
-import 'dotenv/config'
+
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '.env.test' })
+} else {
+  dotenv.config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
@@ -9,9 +15,9 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false) {
-  console.error('⚠️ Invalid environment variables!', _env.error.format())
-  throw new Error('Invalid environment variables.')
+if (!_env.success) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import {app} from './app'
 import { env } from '../env/index';
 
-app.listen ({ port: env.PORT }).then (() =>{
+app.
+listen ({ port: env.PORT 
+})
+.then (() =>{
     console.log(`🚀 Server running on port ${env.PORT}`)
 })
